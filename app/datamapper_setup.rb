710b6cda9814587fac38bc1087b6bdf18e3aca67
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-03-21 16:29:05 +0000

    Added development & database_url for ENV

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -2,7 +2,8 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 require './app/models/user'
+require './app/models/space'
 
-DataMapper.setup(:default, "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
