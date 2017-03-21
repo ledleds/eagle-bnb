d1b744d5fefaf3e97c38500d576b1b3a0cbdea32
Bernard Malhame <bernard.malhame@gmail.com>
2017-03-21 17:46:26 +0000

    user sign out test

---
+++ spec/features/user_sign_out_spec.rb
@@ -0,0 +1,11 @@
+feature 'User sign out' do
+
+   scenario 'while being signed in as a user' do
+     create_user
+     sign_in(username: 'terryeagle', password: 'test')
+     click_button 'Sign Out'
+     expect(page).to have_content('Goodbye!')
+     expect(page).not_to have_content('Welcome, terryeagle')
+
+   end
+end
