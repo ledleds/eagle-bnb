34b0acb7dde1d8ed0e92c5a0daef48f5e9f3f7d2
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-22 16:08:43 +0000

    Remove unnecessary helper method and add test for checking cant list without logging in

--- spec/features/helpers/user_helpers.rb
+++ spec/features/helpers/user_helpers.rb
@@ -46,10 +46,3 @@ def list_space(title: 'The Crows Nest',
   fill_in :date_to, with: date_to
   click_button 'List my space'
 end
-
-def view_spaces
-  click_list_space
-  list_space
-  click_link 'See Spaces'
-  click_link 'Request booking'
-end
