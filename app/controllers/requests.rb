1d7016a0d406be5d614866fb5f9cd4cbc21fb6da
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-23 15:12:18 +0000

    Pass validation for booking properties outside of available date

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -1,13 +1,18 @@
+# require './app'
 class MakersBnB < Sinatra::Base
 
-
-
   post '/submit-request' do
-    request = Request.create(request_date_from: params[:request_date_from],
-                        request_date_to: params[:request_date_to],
-                        user_id: current_user.id,
-                        space_id: current_space.id)
-    redirect '/request_sent'
+    if available?(params[:request_date_from], params[:request_date_to])
+      request = Request.create(request_date_from: params[:request_date_from],
+                          request_date_to: params[:request_date_to],
+                          user_id: current_user.id,
+                          space_id: current_space.id)
+      redirect '/request_sent'
+    else
+      flash.now[:errors] = ['The property is not available for your chosen dates.']
+      erb :'requests/request'
+    end
+
   end
 
   get '/request_sent' do
@@ -17,5 +22,4 @@ class MakersBnB < Sinatra::Base
   get '/request' do
     erb :'requests/request'
   end
-
 end
--- spec/features/request_space_spec.rb
+++ spec/features/request_space_spec.rb
@@ -11,14 +11,14 @@ feature 'Requesting spaces' do
 
   scenario 'user can request a space' do
     expect(page).to have_content('Please select arrival and departure dates.')
-    fill_in :request_date_from, with: '2017-01-27'
-    fill_in :request_date_to, with: '2017-01-28'
+    fill_in :request_date_from, with: Date.new(2017,1,27)
+    fill_in :request_date_to, with: Date.new(2017,1,28)
     click_button 'Request'
   end
 
   scenario 'user can\'t request a space if it is unavailable' do
-    fill_in :request_date_from, with: '2017-01-24'
-    fill_in :request_date_to, with: '2017-01-25'
+    fill_in :request_date_from, with: Date.new(2017,1,24)
+    fill_in :request_date_to, with: Date.new(2017,1,25)
     click_button 'Request'
     expect(page).to have_content('The property is not available for your chosen dates.')
   end
