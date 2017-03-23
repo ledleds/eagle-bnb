3424301c7355e0a00174293f08527c30b5e543ae
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-03-23 13:33:41 +0000

    Work in progress

--- spec/features/request_space_spec.rb
+++ spec/features/request_space_spec.rb
@@ -16,4 +16,15 @@ feature 'Requesting spaces' do
     click_button 'Request'
   end
 
+  scenario 'user can\'t request a space if it is unavailable' do
+    fill_in :request_date_from, with: '2017-01-24'
+    fill_in :request_date_to, with: '2017-01-25'
+    click_button 'Request'
+    expect(page).to have_content('The property is not available for your chosen dates.')
+  end
+
+  # scenario 'user can\'t request a space if it is already booked' do
+  #   click_button 'Request'
+  # end
+
 end
